152c7e430e5aecab6befa4dd9561fd0b3316fb3f
Cristian Feldkircher <102227592+cristianfelk@users.noreply.github.com>
2024-08-21 14:25:11 -0300

    Update and rename bairroService.js to logradouroService.js

---
+++ back-end/src/services/logradouroService.js
@@ -0,0 +1,53 @@
+const db = require('../configs/pg')
+
+const postLogradouro = async (params) => {
+    try {
+        const sql_post = ` insert into logradouro (municipio_id, cep, logradouro, complemento, bairro)
+            values (${params.municipio_id}, 
+                     '${params.cep}',
+                     '${params.logradouro}',
+                     '${ṕarams.complemento}',
+                     '${params.bairro}')`
+        await db.query(sql_post)
+    } catch (error) {
+
+    }
+}
+
+const getLogradouro = async () => {
+    const sql_get = `select * from logradouro`
+    return await db.query(sql_get)
+}
+
+const deleteLogradouro = async (params) => {
+    const sql_delete = `delete from logradouro where id = $1`
+    const { id } = params
+    await db.query(sql_delete, [id])
+}
+
+const putLogradouro = async (params) => {
+    const sql_put = `update logradouro set
+            municipio_id = $2, 
+            cep = $3,
+            logradouro = $4,
+            complemento = $5,
+            bairro = $6
+            where id = $1`
+    const { id, municipio_id, cep, logradouro, complemento, bairro } = params 
+    return await db.query(sql_put, [id, municipio_id, cep, logradouro, complemento, bairro])
+}
+
+const patchLogradouro = async (params) => {
+    let fields = [];
+    Object.keys(params).map(p => p).forEach(e => e !== 'id' && fields.push(`${e} = '${params[e]}'`));
+    fields = fields.join(', ');
+    console.log(params)
+    const sql = `update logradouro set ${fields} where id = ${params.id}`;
+    await db.query(sql);
+}
+
+module.exports.postLogradouro = postLogradouro
+module.exports.getLogradouro = getLogradouro
+module.exports.deleteLogradouro = deleteLogradouro
+module.exports.putLogradouro = putLogradouro
+module.exports.patchLogradouro = patchLogradouro
