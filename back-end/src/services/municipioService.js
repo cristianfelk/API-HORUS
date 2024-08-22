48a5a96fa203d3bb6a83960fcb13061ee347e231
cristianfelk <cristianfeldkircher@gmail.com>
2024-08-21 21:55:32 -0300

    feat: criado paginação municipios

--- back-end/src/services/municipioService.js
+++ back-end/src/services/municipioService.js
@@ -2,8 +2,8 @@ const db = require('../configs/pg');
 
 const postMunicipio = async (params) => {
     try {
-        const sql_post = `INSERT INTO municipio (nome, ibge, uf)
-                          VALUES ($1, $2, $3)`;
+        const sql_post = `insert into municipio (nome, ibge, uf)
+                          values ($1, $2, $3)`;
         await db.query(sql_post, [params.nome, params.ibge, params.uf]);
     } catch (error) {
         console.error('Erro ao inserir município:', error);
@@ -13,27 +13,27 @@ const postMunicipio = async (params) => {
 
 const getMunicipio = async (page = 1, limit = 10) => {
     const offset = (page - 1) * limit;
-    const sql_get = `SELECT * FROM municipio ORDER BY id LIMIT $1 OFFSET $2`;
+    const sql_get = `select * from municipio order by nome limit $1 offset $2`;
     return await db.query(sql_get, [limit, offset]);
 };
 
 const getTotalMunicipios = async () => {
-    const sql_count = 'SELECT COUNT(*) AS total FROM municipio';
+    const sql_count = 'select count(*) as total from municipio';
     const result = await db.query(sql_count);
     return parseInt(result.rows[0].total, 10);
 };
 
 const deleteMunicipio = async (id) => {
-    const sql_delete = `DELETE FROM municipio WHERE id = $1`;
+    const sql_delete = `delete from municipio where id = $1`;
     await db.query(sql_delete, [id]);
 };
 
 const putMunicipio = async (params) => {
-    const sql_put = `UPDATE municipio SET
+    const sql_put = `update municipio set
                      nome = $2, 
                      ibge = $3,
                      uf = $4
-                     WHERE id = $1`;
+                     where id = $1`;
     const { id, nome, ibge, uf } = params;
     return await db.query(sql_put, [id, nome, ibge, uf]);
 };
@@ -42,7 +42,7 @@ const patchMunicipio = async (params) => {
     let fields = [];
     Object.keys(params).forEach(e => e !== 'id' && fields.push(`${e} = $${fields.length + 2}`));
     fields = fields.join(', ');
-    const sql = `UPDATE municipio SET ${fields} WHERE id = $1`;
+    const sql = `update municipio set ${fields} where id = $1`;
     const values = [params.id, ...Object.values(params).filter((_, index) => index !== 0)];
     await db.query(sql, values);
 };
