d8e301c8a5c2211b95d8f92d35bd4483bb2233ef
Cristian F <cristian@zettabrasil.com.br>
2024-08-29 09:40:37 -0300

    fix: correção caminho arq

--- back-end/src/runImport.js
+++ back-end/src/runImport.js
@@ -1,6 +1,6 @@
 const { importCSV } = require('./services/importCsvService');
 
 const logradourosCSV = 'C:\\Users\\crist\\OneDrive\\Área de Trabalho\\API-DENGUE\\back-end\\src\\db\\dados_logradouro.csv';
-const municipioCSV = 'C:\\Users\\crist\\OneDrive\\Área de Trabalho\\API-DENGUE\\back-end\\src\\db\\dados_lmunicipio.csv';
+const municipioCSV = 'C:\\Users\\crist\\OneDrive\\Área de Trabalho\\API-DENGUE\\back-end\\src\\db\\dados_municipio.csv';
 
 importCSV(logradourosCSV, municipioCSV);
